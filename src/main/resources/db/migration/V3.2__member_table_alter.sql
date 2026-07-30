643edccc4f4c6a4c42943bb2708ed4821c22b0ea
kimheesang <heesang522@gmail.com>
2026-07-30 15:54:23 +0900

    임시 계정 생성

--- src/main/resources/db/migration/V3.2__member_table_alter.sql
+++ src/main/resources/db/migration/V3.2__member_table_alter.sql
@@ -1,3 +1,2 @@
 ALTER TABLE member
-    ADD deleted_at DATETIME NULL COMMENT '삭제일(탈퇴일)' AFTER updated_at,
-    DROP INDEX uq_member_email;
+    ADD deleted_at DATETIME NULL COMMENT '삭제일(탈퇴일)' AFTER updated_at
